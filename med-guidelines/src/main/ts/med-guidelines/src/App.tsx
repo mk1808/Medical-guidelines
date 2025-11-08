89d2467acee980d0127213b5058739ea963b070d
mk1808 <37344587+mk1808@users.noreply.github.com>
2025-11-08 22:49:45 +0100

    #2 add scss

--- med-guidelines/src/main/ts/med-guidelines/src/App.tsx
+++ med-guidelines/src/main/ts/med-guidelines/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 import { Button, HStack } from '@chakra-ui/react'
 
 function App() {
@@ -32,6 +32,7 @@ function App() {
       <HStack>
       <Button>Click me</Button>
       <Button>Click me</Button>
+      <h1>my color</h1>
     </HStack>
     </>
   )
