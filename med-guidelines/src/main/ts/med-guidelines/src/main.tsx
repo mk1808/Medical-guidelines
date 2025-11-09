280fe7d39f45229d22ff7b760d47a4b8b0684cae
mk1808 <37344587+mk1808@users.noreply.github.com>
2025-11-09 09:48:54 +0100

    #8 add folder structure

--- med-guidelines/src/main/ts/med-guidelines/src/main.tsx
+++ med-guidelines/src/main/ts/med-guidelines/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './app/App.tsx'
 import { Provider } from './components/ui/provider.tsx'
-import './i18n';
+import './i18n.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
