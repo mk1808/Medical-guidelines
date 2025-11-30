6b8347db726c63ff30c05fd883c0c1fe09c089df
Marek Czopor <marqos111222@gmail.com>
2025-11-30 20:52:40 +0100

    #9 Add auto build frontend

--- med-guidelines/src/main/java/pl/iimo/med_guidelines/config/WebConfig.java
+++ med-guidelines/src/main/java/pl/iimo/med_guidelines/config/WebConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import pl.iimo.med_guidelines.config.filters.SpaFallbackFilter;
+
 @Configuration
 public class WebConfig {
 
--- med-guidelines/src/main/java/pl/iimo/med_guidelines/config/filters/SpaFallbackFilter.java
+++ med-guidelines/src/main/java/pl/iimo/med_guidelines/config/filters/SpaFallbackFilter.java
@@ -1,4 +1,4 @@
-package pl.iimo.med_guidelines.config;
+package pl.iimo.med_guidelines.config.filters;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
